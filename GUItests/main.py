d8bd6e89056e969f8a74ef88a8a3608ade7cfe83
rami <ijoacquin@gmail.com>
2023-11-09 17:48:47 +0800

    changing kivy gui to kivymd

--- GUItests/main.py
+++ GUItests/main.py
@@ -1,6 +1,12 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.core.window import Window
+class MainWindow(MDApp):
+    def build(self):
+        Window.size = (1920, 1080)
+        return Builder.load_file('my.kv')
 
-class Myapp(MDApp):
-    pass
+if __name__ == "__main__":
+    MainWindow().run()
 
-Myapp().run()
+
