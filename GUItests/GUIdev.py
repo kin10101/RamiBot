783e51d7969c5fa6316baa74fa726513f4f57960
kin10101 <ijoacquin@gmail.com>
2023-12-17 20:22:57 +0800

    gui stuff

--- GUItests/GUIdev.py
+++ GUItests/GUIdev.py
@@ -15,14 +15,32 @@ class MainWindow(MDApp):
         global screen_manager
         screen_manager = ScreenManager()
 
-        # ADD ALL SCREENS HERE
-        screen_manager.add_widget(Builder.load_file('mainscreen.kv'))
+        # ADD ALL SCREENS TO BE USED HERE
+        screen_manager.add_widget(Builder.load_file('ChatbotGUI.kv'))
 
         return screen_manager
 
     def change_screen(self, screen_name):
         screen_manager.current = screen_name
 
+    def update_labels(self):
+        '''Update labels in mapscreen'''
+        mapscreen = self.root.get_screen('mapscreen')
+        main_layout = mapscreen.ids.main_layout
+        info_layout = mapscreen.ids.info_layout  # Correct way to access info_layout
+
+        title_label = mapscreen.ids.title
+        descr_label = mapscreen.ids.descr
+        floor_label = mapscreen.ids.floor
+        sched_label = mapscreen.ids.sched
+        image = mapscreen.ids.bg_img
+
+        image.source = "Assets/bg.png"
+        descr_label.text = "dummy text"
+        floor_label.text = "dummy text"
+        sched_label.text = "dummy text"
+        title_label.text = "dummy text"
+
 if __name__ == "__main__":
-    LabelBase.register(name='Poppins', fn_regular="Assets/Poppins-Regular.otf")
+    LabelBase.register(name='Poppins', fn_regular="Assets/Poppins-Regular.otf") # register fonts for use in app
     MainWindow().run()
