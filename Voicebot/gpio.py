cd824993f30d6db18aeb49b5183bfa37d04f7e44
rami <ijoacquin@gmail.com>
2024-01-04 13:00:36 +0800

    added gpio function

--- Voicebot/gpio.py
+++ Voicebot/gpio.py
@@ -3,10 +3,19 @@ try:
 except RuntimeError:
     import fake_rpigpio.RPi as GPIO
 
+
 def set_gpio_pin(pin, state):
     # Set mode and pin
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
-
     # Set the pin to the specified state
     GPIO.output(pin, state)
+
+
+#Usage
+
+'''
+set_gpio_pin(4, GPIO.HIGH)
+time.sleep(5)
+GPIO.cleanup()
+'''
