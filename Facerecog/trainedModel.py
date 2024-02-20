dfd4ea10a274f8573bbc2dfcfc3076e93943a0a1
Marj <comarjorie404@gmail.com>
2024-02-20 22:30:55 +0800

    face recog update - refinement

--- Facerecog/trainedModel.py
+++ Facerecog/trainedModel.py
@@ -4,10 +4,16 @@ from Facerecog import main as m
 facedetect = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 recognizer = cv2.face.LBPHFaceRecognizer.create()
-recognizer.read("/home/rami/PycharmProjects/RamiBot/Facerecog/Trainer.yml")
+
+#RPI
+#recognizer.read("/home/rami/PycharmProjects/RamiBot/Facerecog/Trainer.yml")
+
+#laptop
+recognizer.read("D:\RamiBot Project\RamibotReal\Facerecog\Trainer.yml")
 count = 0
 currentID = 0
 global confidence_result
+
 
 def face_recognition(video):
     global confidence_result
@@ -16,12 +22,13 @@ def face_recognition(video):
     while running:
         ret, frame = video.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facedetect.detectMultiScale(gray, 1.3, 5)
+        gray_eq = cv2.equalizeHist(gray)
+        faces = facedetect.detectMultiScale(gray_eq, scaleFactor=1.1, minNeighbors=8, minSize=(30, 30))
         for (x, y, w, h) in faces:
-            serial, conf = recognizer.predict(gray[y:y + h, x:x + w])
-            currentID = serial
+            serial, conf = recognizer.predict(gray_eq[y:y + h, x:x + w])
             confidence_result = conf
-            if conf > 70:
+
+            if conf > 90:
 
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 2)
@@ -38,9 +45,6 @@ def face_recognition(video):
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 2)
                 cv2.rectangle(frame, (x, y), (x + w, y), (50, 50, 255), 1)
-
-        #frame = cv2.resize(frame, (640, 480))
-        #cv2.imshow('frame', frame)
 
         k = cv2.waitKey(1)
 
