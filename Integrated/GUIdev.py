14c4112b0c3da18d0db0f298256a0bb2e154dcbf
rami <ijoacquin@gmail.com>
2024-04-25 15:58:04 +0800

    test gui

--- Integrated/GUIdev.py
+++ Integrated/GUIdev.py
@@ -255,7 +255,7 @@ class MainApp(MDApp):
                 put_in_queue(image_queue, 'rami_faces/smile.png')
 
     def set_face_blink(self):
-        Clock.schedule_interval(self.await_face_change, .5)
+        Clock.schedule_interval(self.await_face_change, .8)
         Clock.schedule_interval(self.face_blink, 3)
         if screen_manager.current != 'idlescreen':
             Clock.unschedule(self.face_blink)
@@ -307,7 +307,7 @@ class MainApp(MDApp):
             nickname = self.get_text('adduserscreen', 'nickname')
             role = self.get_text('adduserscreen', 'role')
 
-            if not all([user _ID, given_name, last_name, nickname, role]):  # Check if any of the variables are empty
+            if not all([user_ID, given_name, last_name, nickname, role]):  # Check if any of the variables are empty
                 raise ValueError("Empty fields detected")
 
             DataCollector.add_to_db(user_ID, nickname, last_name, given_name, middle_initial, role)
