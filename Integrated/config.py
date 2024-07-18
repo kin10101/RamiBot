105f95a541361aa3273fdc660d49199491e41520
kin <ijoacquin@gmail.com>
2024-07-18 11:59:28 +0800

    added suggestion button image

--- Integrated/config.py
+++ Integrated/config.py
@@ -1,9 +1,10 @@
+# Description: This file contains the configuration settings for the application.
+
+# MainApp.py
 HOST_IP = 'http://192.168.80.4:5000'
 # HOST_IP = "http://192.168.254.169:5000" # laptop IP
-
 IMAGE_PATH = 'downloaded_image.jpg'  # Define a constant path for the image to prevent storage bloat
 REQUEST_TIMEOUT = 2
 TIMEOUT_DURATION = 20
 ANNOUNCEMENT_INTERVAL = 120
 CAMERA_INDEX = 2
-halign = "center"
